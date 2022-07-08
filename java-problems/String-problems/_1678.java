0d7120d9e0aeab1836f07b62986c0fd9d4496248
sambidhan85 <84292806+sambidhan85@users.noreply.github.com>
2022-07-07 18:50:33 -0700

    java problem 1678

---
+++ java-problems/String-problems/_1678.java
@@ -0,0 +1,61 @@
+/*
+1678. Goal Parser Interpretation
+Easy
+
+797
+
+66
+
+Add to List
+
+Share
+You own a Goal Parser that can interpret a string command. The command consists of an alphabet of "G", "()" and/or "(al)" in some order. The Goal Parser will interpret "G" as the string "G", "()" as the string "o", and "(al)" as the string "al". The interpreted strings are then concatenated in the original order.
+
+Given the string command, return the Goal Parser's interpretation of command.
+
+ 
+
+Example 1:
+
+Input: command = "G()(al)"
+Output: "Goal"
+Explanation: The Goal Parser interprets the command as follows:
+G -> G
+() -> o
+(al) -> al
+The final concatenated result is "Goal".
+Example 2:
+
+Input: command = "G()()()()(al)"
+Output: "Gooooal"
+Example 3:
+
+Input: command = "(al)G(al)()()G"
+Output: "alGalooG"
+ 
+
+Constraints:
+
+1 <= command.length <= 100
+command consists of "G", "()", and/or "(al)" in some order. 
+*/
+
+class Solution {
+    public String interpret(String command) {
+        StringBuilder output = new StringBuilder();
+        int i = 0;
+        while(i < command.length()) {
+            if (command.charAt(i) == 'G') {
+                output.append("G");
+                i++;
+            } else if (command.charAt(i) == '(' && command.charAt(i + 1) == ')') {
+                output.append("o");
+                i += 2;
+            } else {
+                output.append("al");
+                i += 4;
+            }
+        }
+        return output.toString();
+    }
+}
